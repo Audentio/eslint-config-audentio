426207988c8439724c92ec5dd47cccea49487187
Tushar Singh <tusharsingh@gmx.com>
2017-01-30 11:33:49 +0530

    Update rules

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ module.exports = {
     },
     'env': {
         'node': true,
+        'browser': true,
     },
     'rules': {
         'import/no-unresolved': ['off'],
         'no-underscore-dangle': ['off'],
-        'camelcase': ["off"],
-        'max-len': ["error", { "code": 150 ,"ignoreComments": true }],
+        'camelcase': ['off'],
+        'max-len': 'off',
         'no-restricted-syntax': [
             2,
             'DebuggerStatement',
@@ -25,17 +26,47 @@ module.exports = {
         'prefer-template': ['off'],
         'no-prototype-builtins': ['off'],
 
+        'react/sort-comp': {
+            order: [
+                'static-methods',
+                'lifecycle',
+                'everything-else',
+                'render'
+            ],
+            groups: {
+                lifecycle: [
+                    'displayName',
+                    'propTypes',
+                    'contextTypes',
+                    'childContextTypes',
+                    'mixins',
+                    'statics',
+                    'defaultProps',
+                    'constructor',
+                    'getDefaultProps',
+                    'getInitialState',
+                    'state',
+                    'getChildContext',
+                    'componentWillMount',
+                    'componentDidMount',
+                    'componentWillReceiveProps',
+                    'shouldComponentUpdate',
+                    'componentWillUpdate',
+                    'componentDidUpdate',
+                    'componentWillUnmount'
+                ],
+            },
+        },
         'react/no-unescaped-entities': ['off'],
         'react/prop-types': 'off',
         'react/forbid-prop-types': ['off'],
         'react/jsx-indent': ['error', 4],
+        'react/require-default-props': 'off',
         'react/jsx-indent-props': ['error', 4],
         'react/no-unused-prop-types': ['off'],
         'react/prefer-stateless-function': 'off',
-        'react/jsx-pascal-case': 'off',
         'jsx-a11y/no-static-element-interactions': ['off'],
 
-        'newline-per-chained-call': 'off',
         'guard-for-in': 'off',
         'no-plusplus': ['off'],
         'arrow-parens': ['off'],
